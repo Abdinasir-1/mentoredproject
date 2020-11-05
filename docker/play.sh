a19f026d0c231e7ddd1eedb2bea2aaab648feb5b
Abdinasir-1 <71330974+Abdinasir-1@users.noreply.github.com>
2020-11-05 21:32:05 +0000

    Update play.sh

--- docker/play.sh
+++ docker/play.sh
@@ -1,7 +1,4 @@
 #! /bin/bash
-mkdir -p ~/.local/bin
-echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
-source ~/.bashrc
 ANSIBLE_SKIP_CONFLICT_CHECK=1 pip install --user ansible
 ansible --version
 ansible-playbook playbook.yaml
