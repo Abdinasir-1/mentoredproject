98576f6edd378a3ba876d5ff9d8dca33de5ef52b
Abdinasir-1 <71330974+Abdinasir-1@users.noreply.github.com>
2020-11-06 11:30:44 +0000

    Update play.sh

--- docker/play.sh
+++ docker/play.sh
@@ -1,12 +1,4 @@
 #! /bin/bash
-mkdir -p ~/.local/bin
-echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
-source ~/.bashrc
-sudo apt install ansible -y
+
 ansible-playbook playbook.yaml
 
-sudo apt update
-sudo apt install -y curl jq
-version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
-sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
